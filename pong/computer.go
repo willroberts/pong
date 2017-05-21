bc3db0b7ee08e8b2b47b09cac15985920727b020
Will Roberts <will.roberts11@gmail.com>
2017-05-21 12:04:29 -0700

    Adjusts comment

--- pong/computer.go
+++ pong/computer.go
@@ -1,10 +1,10 @@
 package pong
 
 // processAI moves the computer-controlled opponent in response to the ball's
-// vertical position.
-// A perfect (unbeatable) AI would start adjusting its position as soon as
-// the ball has matched its Y range. In order to make the AI defeatable, we
-// wait until the ball is out of range before adjusting position.
+// vertical position. An undefetable AI would start adjusting its position as
+// soon as the ball has matched its Y range. In order to make the AI
+// defeatable, we wait until the ball is out of range before adjusting
+// position.
 func (g *GameEngine) processAI() {
 	opponent := g.paddles[1]
 	if opponent.Y() > g.ball.Y()-ballHeight {
